2de6423e9aea1b892d919cec78c6b31f7556c85b
Georgy Shelkovy <g.shelkovy@arenadata.io>
2023-11-27 15:29:46 +0500

    Fix backup statistics for leaf partitions (#42)
    gpbackup does not backup statistics for leaf partitions with
    leaf-partition-data option
    
    This patch solves the problem by backuping statistics for all specified
    tables, including leaf partitions, and not just root partitions.
    
    The test for this patch is based on the test for the 85384fa.
    And, as stated in that patch, it removes the unnecessary
    condition when comparing the number of statistics.
    
    Cherry-picked-from: 0ce7382
    (cherry picked from commit 6b5fe21d6d592ab37d554ad5593308aebadf61c6)

--- backup/wrappers.go
+++ backup/wrappers.go
@@ -205,7 +205,7 @@ func createBackupDirectoriesOnAllHosts() {
  * Metadata retrieval wrapper functions
  */
 
-func RetrieveAndProcessTables() ([]Table, []Table) {
+func RetrieveAndProcessTables() ([]Table, []Table, []Table) {
 	includedRelations := GetIncludedUserTableRelations(connectionPool, IncludedRelationFqns)
 	tableRelations := ConvertRelationsOptionsToBackup(includedRelations)
 
@@ -215,12 +215,12 @@ func RetrieveAndProcessTables() ([]Table, []Table) {
 		tableRelations = append(tableRelations, GetForeignTableRelations(connectionPool)...)
 	}
 
-	tables := ConstructDefinitionsForTables(connectionPool, tableRelations)
+	allTables := ConstructDefinitionsForTables(connectionPool, tableRelations)
 
-	metadataTables, dataTables := SplitTablesByPartitionType(tables, IncludedRelationFqns)
+	metadataTables, dataTables := SplitTablesByPartitionType(allTables, IncludedRelationFqns)
 	objectCounts["Tables"] = len(metadataTables)
 
-	return metadataTables, dataTables
+	return metadataTables, dataTables, allTables
 }
 
 func retrieveFunctions(sortables *[]Sortable, metadataMap MetadataMap) ([]Function, map[uint32]FunctionInfo) {
