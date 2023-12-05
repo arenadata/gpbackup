c581c7c85152adf77b7fb6c8ad06aed13164edb2
Vasiliy Ivanov <ivi@arenadata.io>
2023-12-05 16:51:55 +0200

    Revert "Improve performance of index rename query"
    This reverts commit e0a45f9966608677885f6631ae21298c5ca02f9c.

--- backup/queries_postdata.go
+++ backup/queries_postdata.go
@@ -254,10 +254,12 @@ func GetRenameExchangedPartitionQuery(connection *dbconn.DBConn) string {
 	// tables.  In GPDB6 and below this renaming was done automatically by server code.
 	cteClause := ""
 	if connectionPool.Version.Before("7") {
-		cteClause = `SELECT c.relname
-             FROM pg_class c
-             INNER JOIN pg_partition_rule p
-             ON c.oid = p.parchildrelid`
+		cteClause = `SELECT DISTINCT cl.relname
+            FROM pg_class cl
+                INNER JOIN pg_partitions pts
+					ON cl.relname = pts.partitiontablename
+					AND cl.relname != pts.tablename
+            WHERE cl.relkind IN ('r', 'f')`
 	} else {
 		cteClause = `SELECT DISTINCT cl.relname
              FROM pg_class cl
