ab6aa7496268866def0b23b5cd90c474936d338a
Vasiliy Ivanov <ivi@arenadata.io>
2024-05-05 21:53:29 +0200

    Revert "Rework patch 11da7e0 that solves function-table type dependency issue (#57)"
    This reverts commit c0e62666266e1d85543f7e5e38c25d17e4abba2b.

--- backup/queries_functions.go
+++ backup/queries_functions.go
@@ -36,7 +36,6 @@ type Function struct {
 	NumRows           float32 `db:"prorows"`
 	DataAccess        string  `db:"prodataaccess"`
 	Language          string
-	IsDependOnTables  bool   `db:"prodep"`
 	Kind              string `db:"prokind"`     // GPDB 7+
 	PlannerSupport    string `db:"prosupport"`  // GPDB 7+
 	IsWindow          bool   `db:"proiswindow"` // before 7
@@ -103,12 +102,6 @@ func GetFunctionsAllVersions(connectionPool *dbconn.DBConn) []Function {
 }
 
 func GetFunctions(connectionPool *dbconn.DBConn) []Function {
-	isDependOnTablesClause := `EXISTS (
-		SELECT c.oid
-		FROM pg_class c
-		JOIN pg_type t ON t.oid IN (SELECT unnest(p.proargtypes||p.prorettype))
-		WHERE c.relkind = 'r' AND c.reltype IN (t.oid, t.typelem)
-	) AS prodep`
 	excludeImplicitFunctionsClause := ""
 	if connectionPool.Version.AtLeast("6") {
 		// This excludes implicitly created functions. Currently this is only range type functions
@@ -147,15 +140,14 @@ func GetFunctions(connectionPool *dbconn.DBConn) []Function {
 			procost,
 			prorows,
 			prodataaccess,
-			l.lanname AS language,
-			%s
+			l.lanname AS language
 		FROM pg_proc p
 			JOIN pg_catalog.pg_language l ON p.prolang = l.oid
 			LEFT JOIN pg_namespace n ON p.pronamespace = n.oid
 		WHERE %s
 			AND proisagg = 'f'
 			AND %s%s
-		ORDER BY nspname, proname, identargs`, locationAtts, isDependOnTablesClause,
+		ORDER BY nspname, proname, identargs`, locationAtts,
 		SchemaFilterClause("n"),
 		ExtensionFilterClause("p"),
 		excludeImplicitFunctionsClause)
@@ -181,7 +173,6 @@ func GetFunctions(connectionPool *dbconn.DBConn) []Function {
 			prokind,
 			prosupport,
 			l.lanname AS language,
-			%s
             coalesce(array_to_string(ARRAY(SELECT 'FOR TYPE ' || nm.nspname || '.' || typ.typname
                 from
                     unnest(p.protrftypes) as trf_unnest
@@ -196,7 +187,7 @@ func GetFunctions(connectionPool *dbconn.DBConn) []Function {
 		WHERE %s
 			AND prokind <> 'a'
 			AND %s%s
-		ORDER BY nspname, proname, identargs`, locationAtts, isDependOnTablesClause,
+		ORDER BY nspname, proname, identargs`, locationAtts,
 		SchemaFilterClause("n"),
 		ExtensionFilterClause("p"),
 		excludeImplicitFunctionsClause)
