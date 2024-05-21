0435638a7609d10a48876cef1b7ad87a9f8d05b6
Roman Eskin <r.eskin@arenadata.io>
2024-05-22 00:03:30 +1000

    Fix duplicated column definition in the gpbackup metadata (#83)
    Problem description:
    If a column in a table had privileges granted for two (or more) users, gpbackup
    created metadata file, where this column was duplicated.
    
    Cause:
    The SQL query, that is used to retrieve information about all table columns in
    the function 'GetColumnDefinitions', had the 'unnest' function applied to the
    array of column-level access privileges from the 'pg_catalog.pg_attribute'.
    Because of the 'unnest', if there were several aclitems for a column, the result
    of the query had several rows returned for this column. But gpbackup treated all
    these rows as results for separate columns, causing duplication in the metadata.
    
    Fix:
    'unnest' is removed in the query, 'attacl' is returned as is. Thus, for each
    column, there is only one row in the result of the query. Now, the field
    'Privileges' in the 'ColumnDefinition' struct contains all column-level access
    privileges in a slice of strings ('pq.StringArray'). And later, in
    'getColumnACL', this slice, where each string contains a separate aclitem, is
    iterated and each item is parsed as before. Plus, the 'Kind' field is removed
    from the 'ColumnDefinition' struct.
    
    (cherry picked from commit f65a1893cc3fd0ca5db968e649d051ab19c40c4d)

--- integration/predata_relations_create_test.go
+++ integration/predata_relations_create_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/greenplum-db/gpbackup/backup"
 	"github.com/greenplum-db/gpbackup/testutils"
 	"github.com/greenplum-db/gpbackup/toc"
+	"github.com/lib/pq"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -425,7 +426,7 @@ SET SUBPARTITION TEMPLATE ` + `
 		})
 		It("prints column level privileges", func() {
 			testutils.SkipIfBefore6(connectionPool)
-			privilegesColumnOne := backup.ColumnDefinition{Oid: 0, Num: 1, Name: "i", Type: "integer", StatTarget: -1, Privileges: sql.NullString{String: "testrole=r/testrole", Valid: true}}
+			privilegesColumnOne := backup.ColumnDefinition{Oid: 0, Num: 1, Name: "i", Type: "integer", StatTarget: -1, Privileges: pq.StringArray{"testrole=r/testrole", "anothertestrole=r/testrole"}}
 			tableMetadata.Owner = "testrole"
 			testTable.ColumnDefs = []backup.ColumnDefinition{privilegesColumnOne}
 			backup.PrintPostCreateTableStatements(backupfile, tocfile, testTable, tableMetadata, []uint32{0, 0})
