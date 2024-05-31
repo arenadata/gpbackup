5a3a3c0f7cf1f21d2eecdee468b5dab019c62773
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-05-31 08:45:17 +0500

    encode statistics by batches

--- toc/toc.go
+++ toc/toc.go
@@ -18,7 +18,7 @@ type TOC struct {
 	GlobalEntries       []MetadataEntry
 	PredataEntries      []MetadataEntry
 	PostdataEntries     []MetadataEntry
-	StatisticsEntries   []MetadataEntry
+	StatisticsEntries   []MetadataEntry `yaml:",omitempty"`
 	DataEntries         []CoordinatorDataEntry
 	IncrementalMetadata IncrementalEntries
 }
@@ -142,14 +142,38 @@ func NewSegmentTOC(filename string) *SegmentTOC {
 	return toc
 }
 
+func encodeMetadataEntry(enc *yaml.Encoder, file *os.File, name string, value []MetadataEntry) {
+	_, err := fmt.Fprintf(file, "%s:", strings.ToLower(name))
+	gplog.FatalOnError(err)
+	length := len(value)
+	if length == 0 {
+		_, err := fmt.Fprintln(file, " []")
+		gplog.FatalOnError(err)
+	} else {
+		_, err := fmt.Fprintln(file, "")
+		gplog.FatalOnError(err)
+		slice := 10000
+		for start := 0; start < length; start += slice {
+			end := start + slice
+			if end > length {
+				end = length
+			}
+			enc.Encode(value[start:end:end])
+		}
+	}
+}
+
 func (toc *TOC) WriteToFileAndMakeReadOnly(filename string) {
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	gplog.FatalOnError(err)
 	defer file.Close()
 	enc := yaml.NewEncoder(file)
 	defer enc.Close()
+	StatisticsEntries := toc.StatisticsEntries
+	toc.StatisticsEntries = nil
 	err = enc.Encode(toc)
 	gplog.FatalOnError(err)
+	encodeMetadataEntry(enc, file, "StatisticsEntries", StatisticsEntries)
 	err = file.Chmod(0444)
 	gplog.FatalOnError(err)
 	err = file.Sync()
