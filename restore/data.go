3cfdc87ed0c33b68a258201c5ac5304855c899f6
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-11-02 15:35:05 +0500

    Make copy command cancelable
    If the command used in copy does not support handling of SIGINT signal, then
    such copy command cannot be canceled. timeout command does not handle SIGINT
    signal by default, so its use in gprestore makes copy command cannot be
    canceled until timeout command completes. This patch adds foreground argument
    to timeout command so that it can handle SIGINT signal, and thus copy command
    can be canceled while timeout command is running.

--- restore/data.go
+++ restore/data.go
@@ -40,7 +40,7 @@ func CopyTableIn(queryContext context.Context, connectionPool *dbconn.DBConn, ta
 		//helper.go handles compression, so we don't want to set it here
 		customPipeThroughCommand = utils.DefaultPipeThroughProgram
 		errorFile := fmt.Sprintf("%s_error", globalFPInfo.GetSegmentPipePathForCopyCommand())
-		readFromDestinationCommand = fmt.Sprintf("(timeout 300 bash -c \"while [[ ! -p \"%s\" && ! -f \"%s\" ]]; do sleep 1; done\" || (echo \"Pipe not found %s\">&2; exit 1)) && %s", destinationToRead, errorFile, destinationToRead, readFromDestinationCommand)
+		readFromDestinationCommand = fmt.Sprintf("(timeout --foreground 300 bash -c \"while [[ ! -p \"%s\" && ! -f \"%s\" ]]; do sleep 1; done\" || (echo \"Pipe not found %s\">&2; exit 1)) && %s", destinationToRead, errorFile, destinationToRead, readFromDestinationCommand)
 	} else if MustGetFlagString(options.PLUGIN_CONFIG) != "" {
 		readFromDestinationCommand = fmt.Sprintf("%s restore_data %s", pluginConfig.ExecutablePath, pluginConfig.ConfigPath)
 	}
