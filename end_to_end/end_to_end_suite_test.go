39a7a3ac1cd1d9f51957717f110f95a130104fd2
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-12-04 20:46:31 +0500

    fix test

--- end_to_end/end_to_end_suite_test.go
+++ end_to_end/end_to_end_suite_test.go
@@ -2607,8 +2607,6 @@ LANGUAGE plpgsql NO SQL;`)
 			Expect(err).To(HaveOccurred())
 			Expect(string(output)).To(MatchRegexp(`Error loading data into table public.t1: COPY t1, line \d+, column i: "\d+": ERROR: value "\d+" is out of range for type smallint`))
 			Expect(string(output)).To(ContainSubstring(`Error loading data into table public.t2: timeout: context canceled`))
-			Expect(string(output)).To(ContainSubstring(`Error loading data into table public.t1`))
-			Expect(string(output)).To(ContainSubstring(`is out of range for type smallint`))
 			assertDataRestored(restoreConn, map[string]int{
 				"public.t0": 0,
 				"public.t1": 0,
@@ -2628,14 +2626,14 @@ LANGUAGE plpgsql NO SQL;`)
 				"--resize-cluster", "--jobs", "3", "--on-error-continue")
 			output, err := gprestoreCmd.CombinedOutput()
 			Expect(err).To(HaveOccurred())
-			Expect(string(output)).To(ContainSubstring(`Error loading data into table public.t1`))
-			Expect(string(output)).To(ContainSubstring(`Error loading data into table public.t3`))
-			Expect(string(output)).To(ContainSubstring(`is out of range for type smallint`))
+			Expect(string(output)).To(MatchRegexp(`Error loading data into table public.t1: COPY t1, line \d+, column i: "\d+": ERROR: value "\d+" is out of range for type smallint`))
+			Expect(string(output)).To(MatchRegexp(`Error loading data into table public.t3: COPY t3, line \d+, column i: "\d+": ERROR: value "\d+" is out of range for type smallint`))
 			assertDataRestored(restoreConn, map[string]int{
+				"public.t0": 1000000,
 				"public.t1": 0,
-				"public.t2": 0,
+				"public.t2": 1000000,
 				"public.t3": 0,
-				"public.t4": 0})
+				"public.t4": 1000000})
 			assertArtifactsCleaned("20240502095933")
 			testhelper.AssertQueryRuns(restoreConn, "DROP TABLE t0; DROP TABLE t1; DROP TABLE t2; DROP TABLE t3; DROP TABLE t4;")
 		})
