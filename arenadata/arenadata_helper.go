2c3eddb59d3807fea8d64958c7a2a8839582136a
Viktor Kurilko <v.kurilko@arenadata.io>
2025-08-21 14:46:12 +0700

    Fix inefficient sql for restore statistics
    Statistics are restored in 3 stages. In the first step, we update reltuples in
    pg_class. In the second step, we delete statistics for a specific attribute in
    pg_statistic. And at the last stage, we insert new statistics for this
    attribute. At the second stage, to determine the attribute number, it is
    searched in pg_attribute. For this, a subquery and the IN operator were used.
    This led to an inefficient plan that contains a seq scan on pg_statistic. This,
    in turn, can significantly affect the speed of statistical restore.
    
    Fix this by replacing the IN operator with = in the attribute statistics
    deletion query. We can be sure that the subquery will return no more than one
    row, since pg_attribute has a unique index on the attrelid and attname
    attributes.
    
    Since this fix will only affect new backups, a fix for gprestore has also been
    added. For backups created by problematic versions of gpbackup, nested loop is
    enabled before restoring statistics. This should lead to a more effective plan
    with index scan.

--- arenadata/arenadata_helper.go
+++ arenadata/arenadata_helper.go
@@ -13,22 +13,9 @@ var (
 )
 
 func EnsureAdVersionCompatibility(backupVersion string, restoreVersion string) {
-	var (
-		adBackup, adRestore int
-		err                 error
-	)
-	if strVersion := adPattern.FindAllStringSubmatch(backupVersion, -1); len(strVersion) > 0 {
-		adBackup, err = strconv.Atoi(strVersion[0][1])
-		gplog.FatalOnError(err)
-	} else {
-		gplog.Fatal(errors.Errorf("Invalid arenadata version format for gpbackup: %s", backupVersion), "")
-	}
-	if strVersion := adPattern.FindAllStringSubmatch(restoreVersion, -1); len(strVersion) > 0 {
-		adRestore, err = strconv.Atoi(strVersion[0][1])
-		gplog.FatalOnError(err)
-	} else {
-		gplog.Fatal(errors.Errorf("Invalid arenadata version format for gprestore: %s", restoreVersion), "")
-	}
+	adBackup := GetArenadataVersion(backupVersion)
+	adRestore := GetArenadataVersion(restoreVersion)
+
 	if adRestore < adBackup {
 		gplog.Fatal(errors.Errorf("gprestore arenadata%d cannot restore a backup taken with gpbackup arenadata%d; please use gprestore arenadata%d or later.",
 			adRestore, adBackup, adBackup), "")
@@ -39,4 +26,14 @@ func EnsureAdVersionCompatibility(backupVersion string, restoreVersion string) {
 // example: 1.20.4_arenadata2+dev.1.g768b7e0 -> 1.20.4+dev.1.g768b7e0
 func GetOriginalVersion(fullVersion string) string {
 	return adPattern.ReplaceAllString(fullVersion, "")
+}
+
+func GetArenadataVersion(fullVersion string) uint {
+	match := adPattern.FindStringSubmatch(fullVersion)
+	if len(match) != 2 {
+		gplog.Fatal(errors.Errorf("Invalid arenadata version format for gpbackup: %s", fullVersion), "")
+	}
+	result, err := strconv.ParseUint(match[1], 10, 32)
+	gplog.FatalOnError(err)
+	return uint(result)
 }
