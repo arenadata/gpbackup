arenadata/gpbackup

commit 4bfd4c0095fa14b227692a9e53ebdda6df1e807b

Cleanup in gprestore kills only own helpers (#127)
If several restores of the same backup are launched simultaneously, then the
very first restore that completes during cleanup kills not only its helpers,
but also all helpers working with the same backup, because the processes are
filtered only by same TOC-file. The patch solves the problem by adding a filter
by the name of the pipe, which already has the PID of the main process of the
restore. gpbackup is not affected by this problem, because it will always have
a unique TOC-file.

Ticket: ADBDEV-7175

utils/agent_remote.go
@@ -277,7 +277,9 @@ func CheckHelperPids(c *cluster.Cluster, fpInfo filepath.FilePathInfo, operation
 	remoteOutput := c.GenerateAndExecuteCommand("Checking for leftover gpbackup_helper processes", cluster.ON_SEGMENTS, func(contentID int) string {
 		tocFile := fpInfo.GetSegmentTOCFilePath(contentID)
 		procPattern := fmt.Sprintf("gpbackup_helper --%s-agent --toc-file %s*", operation, tocFile)
-		return fmt.Sprintf(`ps ux | grep "%s" | grep -v grep | awk '{print $2}'`, procPattern)
+		pipeFile := fpInfo.GetSegmentPipeFilePath(contentID)
+		pipePattern := fmt.Sprintf("--pipe-file %s", pipeFile)
+		return fmt.Sprintf(`ps ux | grep "%s" | grep "%s" | grep -v grep | awk '{print $2}'`, procPattern, pipePattern)
 	})
 	pidMap := make(map[string][]int)
 	for _, cmd := range remoteOutput.Commands {
@@ -316,7 +318,9 @@ func CleanUpSegmentHelperProcesses(c *cluster.Cluster, fpInfo filepath.FilePathI
 			c.GenerateAndExecuteCommand("Cleaning up gpbackup_helper processes", cluster.ON_SEGMENTS, func(contentID int) string {
 				tocFile := fpInfo.GetSegmentTOCFilePath(contentID)
 				procPattern := fmt.Sprintf("gpbackup_helper --%s-agent --toc-file %s", operation, tocFile)
-				return fmt.Sprintf("PIDS=`ps ux | grep \"%s\" | grep -v grep | awk '{print $2}'`; if [[ ! -z \"$PIDS\" ]]; then kill -USR1 $PIDS; fi", procPattern)
+				pipeFile := fpInfo.GetSegmentPipeFilePath(contentID)
+				pipePattern := fmt.Sprintf("--pipe-file %s", pipeFile)
+				return fmt.Sprintf("PIDS=`ps ux | grep \"%s\" | grep \"%s\" | grep -v grep | awk '{print $2}'`; if [[ ! -z \"$PIDS\" ]]; then kill -USR1 $PIDS; fi", procPattern, pipePattern)
 			})
 		case <-time.After(timeout):
 			for host, pids := range helperPids {

end_to_end/end_to_end_suite_test.go
@@ -841,6 +841,44 @@ var _ = Describe("backup and restore end to end tests", func() {
 			}
 			Expect(err).NotTo(HaveOccurred())
 		})
+		It(`Ensures that concurrent restores from the same backup will not interfere with each other's helpers`, func() {
+			testhelper.AssertQueryRuns(backupConn,
+				"CREATE TABLE public.t AS SELECT i FROM generate_series(1, 10) i DISTRIBUTED BY (i)")
+			defer testhelper.AssertQueryRuns(backupConn, "DROP TABLE public.t")
+			output := gpbackup(gpbackupPath, backupHelperPath,
+				"--single-data-file", "--include-table", "public.t")
+			timestamp := getBackupTimestamp(string(output))
+			gprestore(gprestorePath, restoreHelperPath, timestamp,
+				"--redirect-db", "restoredb", "--metadata-only")
+			restoreConn.MustExec(
+				"BEGIN; LOCK TABLE public.t IN ACCESS EXCLUSIVE MODE")
+			var backWg sync.WaitGroup
+			errchan := make(chan error, 2)
+			backWg.Add(1)
+			go func() {
+				defer backWg.Done()
+				cmd := exec.Command(gprestorePath, "--timestamp", timestamp,
+					"--redirect-db", "restoredb", "--data-only")
+				_, err := cmd.CombinedOutput()
+				errchan <- err
+			}()
+			backWg.Add(1)
+			go func() {
+				defer backWg.Done()
+				cmd := exec.Command(gprestorePath, "--timestamp", timestamp,
+					"--redirect-db", "restoredb", "--data-only",
+					"--exclude-table", "public.t")
+				_, err := cmd.CombinedOutput()
+				restoreConn.MustExec("COMMIT")
+				errchan <- err
+			}()
+			backWg.Wait()
+			close(errchan)
+			for err := range errchan {
+				Expect(err).ToNot(HaveOccurred())
+			}
+			assertDataRestored(restoreConn, map[string]int{"public.t": 10})
+		})
 		It(`ensures gprestore on corrupt backup with --on-error-continue logs error tables`, func() {
 			if segmentCount != 3 {
 				Skip("Restoring from a tarred backup currently requires a 3-segment cluster to test.")

commit 65603801ccb8efd0a4d1aeeeac45ebcc8cbd1c27

Cleanup never terminates helper processes
The problem is caused by additional grep command that was introduced in
4bfd4c0. Expressions starting with -- should be escaped properly to
returns anything. The command pipeline is failed with:

> grep: unrecognized option '--pipe-file

on stderr and empty stdout. As a result, process cleanup do nothing.

This patch add expression separators for grep command.

Ticket: ADBDEV-8304

utils/agent_remote.go
@@ -279,7 +279,7 @@ func CheckHelperPids(c *cluster.Cluster, fpInfo filepath.FilePathInfo, operation
 		procPattern := fmt.Sprintf("gpbackup_helper --%s-agent --toc-file %s*", operation, tocFile)
 		pipeFile := fpInfo.GetSegmentPipeFilePath(contentID)
 		pipePattern := fmt.Sprintf("--pipe-file %s", pipeFile)
-		return fmt.Sprintf(`ps ux | grep "%s" | grep "%s" | grep -v grep | awk '{print $2}'`, procPattern, pipePattern)
+		return fmt.Sprintf(`ps ux | grep -- "%s" | grep -- "%s" | grep -v grep | awk '{print $2}'`, procPattern, pipePattern)
 	})
 	pidMap := make(map[string][]int)
 	for _, cmd := range remoteOutput.Commands {
@@ -320,7 +320,7 @@ func CleanUpSegmentHelperProcesses(c *cluster.Cluster, fpInfo filepath.FilePathI
 				procPattern := fmt.Sprintf("gpbackup_helper --%s-agent --toc-file %s", operation, tocFile)
 				pipeFile := fpInfo.GetSegmentPipeFilePath(contentID)
 				pipePattern := fmt.Sprintf("--pipe-file %s", pipeFile)
-				return fmt.Sprintf("PIDS=`ps ux | grep \"%s\" | grep \"%s\" | grep -v grep | awk '{print $2}'`; if [[ ! -z \"$PIDS\" ]]; then kill -USR1 $PIDS; fi", procPattern, pipePattern)
+				return fmt.Sprintf("PIDS=`ps ux | grep -- \"%s\" | grep -- \"%s\" | grep -v grep | awk '{print $2}'`; if [[ ! -z \"$PIDS\" ]]; then kill -USR1 $PIDS; fi", procPattern, pipePattern)
 			})
 		case <-time.After(timeout):
 			for host, pids := range helperPids {